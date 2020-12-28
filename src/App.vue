1029ed76099d498f54d7e4d77dadb3bed4f47155
tadex121 <62935149+tadex121@users.noreply.github.com>
2020-12-28 16:38:44 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <TheHeader />
+
+  <div class="row">
+    <EveryItem
+      v-for="item in forSale"
+      :key="item.openId"
+      :openId="item.openId"
+      :name="item.name"
+      :image="item.image"
+      :price="item.price"
+      :text="item.text"
+    />
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader";
+import EveryItem from "./components/EveryItem";
+export default {
+  components: { TheHeader, EveryItem },
+  computed: {
+    forSale() {
+      return this.$store.getters.forSale;
+    },
+    inCart() {
+      return this.$store.getters.inCart;
+    },
+  },
+};
+</script>
+
+<style></style>
