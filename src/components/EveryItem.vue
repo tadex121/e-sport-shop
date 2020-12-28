ebfe3e9a622fb2e03424334fe6773bbc6f798612
tadex121 <62935149+tadex121@users.noreply.github.com>
2020-12-28 16:43:22 +0100

    Create EveryItem.vue

---
+++ src/components/EveryItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="col-md-3">
+    <div class="card-deck px-4 py-4 bg-secondary">
+      <div class="card">
+        <img class="card-img-top" :src="image" alt="Card image cap" />
+        <div class="card-body">
+          <h5 class="card-title">{{ name }}</h5>
+          <p class="card-text">
+            {{ text }}
+          </p>
+          <p class="card-text">{{ price }}€</p>
+          <button
+            class="btn btn-primary btn-lg btn-block"
+            @click="addToCart(openId)"
+          >
+            Add to cart
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EveryItem",
+  props: ["openId", "name", "image", "price", "text"],
+  methods: {
+    addToCart(openId) {
+      this.$store.dispatch("addToCart", openId);
+    },
+  },
+};
+</script>
+
+<style></style>
